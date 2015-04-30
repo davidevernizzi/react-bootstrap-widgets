24b66deb625439a5be2586f69b92128afe66c451
Davide Vernizzi <davide.vernizzi@gmail.com>
2015-04-30 16:19:13 +0200

    first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,8 @@
 "use strict"
+
+var HelloWorld = require('./widgets/helloworld.jsx');
+
+React.render(
+        <HelloWorld />,
+        document.getElementById('content')
+        );
---
+++ src/widgets/helloworld.jsx
@@ -0,0 +1,11 @@
+"use strict"
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <div>Hello, World!</div>
+            );
+    }
+});
+
+module.exports = HelloWorld;
